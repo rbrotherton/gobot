036a2d9e598747e47db6fecd91bba04a65461300
Christopher Johnson <esreborn@gmail.com>
2018-07-01 16:47:26 -0400

    - update gitignore to not commit node_modules  - remove request from package.json  - replace request package with axios in weather and define commands  - reduce code

--- commands/define.js
+++ commands/define.js
@@ -1,4 +1,12 @@
-const conf = require('../config.json');
+const conf = require('../config.json')
+const axios = require('axios')
+
+// Set the headers
+axios.defaults.headers = {
+    'app_id': conf.apis.define.app_id,
+    'app_key': conf.apis.define.app_key
+}
+
 module.exports = {
     name: 'define',
     description: 'Gets the definition of an English word',
@@ -11,49 +19,24 @@ module.exports = {
 
     	let word = args[0];
     	let url = `${conf.apis.define.host}/entries/${conf.apis.define.language}/${word.toLowerCase()}`;
-    	// console.log(url);
 
-    	// Create request
-		var request = require('request');
-
-		// Set the headers
-		var headers = {
-		    'app_id': conf.apis.define.app_id,
-		    'app_key': conf.apis.define.app_key
-		}
-
-		// Configure the request
-		var options = {
-		    url: url,
-		    method: 'GET',
-		    headers: headers,
-		    // qs: {'key1': 'xxx', 'key2': 'yyy'}
-		}
-
-		// Start the request
-		request(options, function (error, response, body) {
-		    if (!error && response.statusCode == 200) {
-
-		    	try {
-		    		let obj = JSON.parse(body);
-			    	let lexEntry = obj.results[0].lexicalEntries[0];
-			    	let type 	 = lexEntry.lexicalCategory;
-			    	let entry 	 = lexEntry.entries[0];
-			    	let def 	 = entry.senses[0].definitions[0];
-			    	// let notes 	 = entry.senses[0].notes[0];
-			    	message.reply(`**${word}** (${type}): ${def}`);	
-		    	}
-		    	catch(error) {
-		    		message.reply("There was an error parsing the response.");	
-		    	}
-
-		        
-		    } else {
-		    	// TODO Better errors
-		    	message.reply("There was an error looking up that word.");
-		    }
-		})
-
-    },
+        axios.get(url).then((response) =>{
+            try {
+                let lexEntry = response.data.results[0].lexicalEntries[0]
+                let type 	 = lexEntry.lexicalCategory;
+                let entry 	 = lexEntry.entries[0];
+                let def 	 = entry.senses[0].definitions[0];
+                // let notes 	 = entry.senses[0].notes[0];
+                message.reply(`**${word}** (${type}): ${def}`);
+            }
+            catch(error) {
+                message.reply("There was an error parsing the response.");
+            }
+        }, (error) => {
+            // TODO Better errors
+            console.info(error)
+            message.reply("There was an error looking up that word.");
+        })
+    }
 };
 
