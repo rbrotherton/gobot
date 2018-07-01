e7fa66d4792d474dc228a6c61a255bee4482211d
Ryan Brotherton <rbrotherton@gmail.com>
2018-07-01 16:17:52 -0400

    Add 8ball command

---
+++ commands/8ball.js
@@ -0,0 +1,36 @@
+const conf = require('../config.json');
+module.exports = {
+    name: '8ball',
+    description: 'Gives a random answer to a question',
+    args: false,
+    usage: '',
+    aliases: ['8'],
+    cooldown: 1,
+    execute(message, args) {
+        
+        let answers = [
+            "It is certain", 
+            "It is decidedly so", 
+            "Without a doubt", 
+            "Yes definitely", 
+            "You may rely on it", 
+            "As I see it, yes", 
+            "Most likely", 
+            "Outlook good", 
+            "Yes", 
+            "Signs point to yes", 
+            "Reply hazy try again", 
+            "Ask again later", 
+            "Better not tell you now", 
+            "Cannot predict now", 
+            "Concentrate and ask again", 
+            "Don't count on it", 
+            "My reply is no", 
+            "My sources say no", 
+            "Outlook not so good", 
+            "Very doubtful"
+        ]
+
+        message.reply(answers[Math.floor(Math.random() * answers.length)]);
+    },
+};
