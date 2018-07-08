ebc4f15a5da77e1985670373acf6a390d3a3edf6
Ryan Brotherton <rbrotherton@gmail.com>
2018-07-08 08:42:57 -0400

    8ball command - remove indefinite answers, add enough negative answers to make outcome 50/50

--- commands/8ball.js
+++ commands/8ball.js
@@ -19,16 +19,16 @@ module.exports = {
             "Outlook good", 
             "Yes", 
             "Signs point to yes", 
-            "Reply hazy try again", 
-            "Ask again later", 
-            "Better not tell you now", 
-            "Cannot predict now", 
-            "Concentrate and ask again", 
             "Don't count on it", 
             "My reply is no", 
             "My sources say no", 
             "Outlook not so good", 
-            "Very doubtful"
+            "Very doubtful",
+            "I don't think so",
+            "Definitely not",
+            "No",
+            "Probably not",
+            "Unlikely"
         ]
 
         let reply = answers[Math.floor(Math.random() * answers.length)];        
