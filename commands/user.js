b7b1c54deb531ff330260d36a7109ecf1d7cfc6a
Ryan Brotherton <rbrotherton@gmail.com>
2018-07-23 16:12:22 -0400

    Fixes #16. Allow user lookups other than self.

--- commands/user.js
+++ commands/user.js
@@ -2,7 +2,19 @@ module.exports = {
     name: 'user',
     description: 'Returns info about the requesting user',
     execute(message, args) {
-        let user = message.author;
+
+    	// Which user?
+    	let user = message.author;
+    	if(args.length){
+    		user = message.mentions.users.first();
+
+    		if(null == user){
+    			message.reply("You must mention a user by @name to look up their details");
+    			return;
+    		}
+    	}
+
+    	// Send responses
         let ping = Math.round(user.client.ping);
 		message.reply(`Username: ${user.username} | ID: ${user.id} | Ping: ${ping}`);
     },
