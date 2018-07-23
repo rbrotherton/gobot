adcbc0ea40bdf2dc05778221b8fbb59bda5a8bd9
Ryan Brotherton <rbrotherton@gmail.com>
2018-07-23 15:32:38 -0400

    Adds server ID to .server output

--- commands/server.js
+++ commands/server.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'server',
     description: 'Returns info about the server where this request originated.',
     execute(message, args) {
-        message.reply(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+        message.reply(`Name: ${message.guild.name}\nID: ${message.guild.id}\nTotal members: ${message.guild.memberCount}`);
     },
 };
