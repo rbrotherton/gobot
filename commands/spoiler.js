6f8edbda4fa4c5831aac23ab7b4cd1f438c8cdc6
Ryan Brotherton <rbrotherton@gmail.com>
2018-08-30 14:46:38 -0400

    Refined spoiler command

--- commands/spoiler.js
+++ commands/spoiler.js
@@ -4,12 +4,14 @@ module.exports = {
     name: 'spoiler',
     description: 'Embed a spoiler ',
     args: false,
-    usage: '<spoiler text>',
+    usage: 'topic <spoiler text>',
     aliases: ['spoiler'],
     cooldown: 3,
     execute(message, args) {
 
         // Get input
+        let topic = args[0]
+        args.shift(); // Remove first arg
         let input = args.join(" ");
         let spoiler = `[Hover to View Spoiler](https://www.google.com "${input}")`;
         let author = getAuthorNick(message);
@@ -17,10 +19,9 @@ module.exports = {
         // Create embed
         let embed  = new Discord.RichEmbed();
         embed.setDescription(spoiler);
-        embed.setTitle(`Spoiler by ${author}`);
 
         // Send embed and delete original message
-        message.channel.send("", embed)
+        message.channel.send(`${topic} spoiler by ${author}`, embed)
         	.then(function(new_msg){})
             .catch(error => console.log(error))
         message.delete();
