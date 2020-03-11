99d96eff001d70aa1f701634770b187cfc8131bf
Ryan Brotherton <rbrotherton@gmail.com>
2020-03-11 16:26:43 -0400

    Improved Sotck quote output

--- commands/stock.js
+++ commands/stock.js
@@ -1,5 +1,5 @@
 const conf    = require('../config.json');
-
+const Discord = require('discord.js');
 module.exports = {
     name: 'stock',
     description: 'Gets stock quotes',
@@ -9,49 +9,138 @@ module.exports = {
     cooldown: 1,
     execute(message, args) {
 
-    	getTicker(message, args[0].toUpperCase());
-
-        // Get a Ticker value
-        function getTicker(message, ticker_id) {
-        	
-    		// Configure the request
-            var request = require('request');
-            var options = {
-                url: `${conf.apis.stocks.host}/query?function=TIME_SERIES_INTRADAY&symbol=${ticker_id}&interval=60min&apikey=${conf.apis.stocks.key}&outputsize=compact`,
-                method: 'GET',
-                headers: {},
+        // Define period for time series change data
+        let period = "D";
+        /*if(typeof(args[1]) != "undefined") {
+            let inp_period = args[1].toUpperCase();
+            console.log(inp_period);
+            if(["D", "W", "M"].includes(inp_period)) {
+                period = inp_period;
+                console.log("inp set");
             }
+        }*/
 
-            // Start the request
-            request(options, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
+        // Define variables
+        let ticker_id = args[0].toUpperCase();
+        let chunk        = "";
+        let chunk_index  = "";
+        let previous     = "";
+        let api          = "";
+        let first_day    = new Date();
+        let second_day   = "";
+        let first_stamp  = "";
+        let second_stamp = "";
+            
+        switch(period) {
+            case "D": 
+                second_day = new Date(Date.now() - 86400000);
+                chunk = "Daily";
+                previous = "Yesterday";
+                chunk_index = "Time Series (Daily)";
+                api = "TIME_SERIES_DAILY";
+                break;
 
-                    try {
-                        
-                        let obj    = JSON.parse(body);
-                        let meta   = obj['Meta Data'];
-                        let date   = meta["3. Last Refreshed"];
-                        let series = obj["Time Series (60min)"];
-                        let data   = series[date];
-                        let close_price = data[Object.keys(data)[3]];
-                        
-                        close_price = parseFloat(close_price).toFixed(2);
+            case "W": 
+                chunk = "Weekly";
+                chunk_index = "Weekly Time Series";
+                previous = "End of Last Week";
+                api = "TIME_SERIES_WEEKLY";
+                second_day = new Date(Date.now() - (86400000 * 7)); // TODO: Needs to be previous friday
+                break;
 
-                        message.reply(`💰 **${ticker_id.toUpperCase()}**: \$${close_price}`); 
-                    }
-                    catch(error) {
-                    	console.log(error);
-                        message.reply("There was an error parsing the response.");  
-                    }
+            case "M": 
+                chunk = "Monthly";
+                chunk_index = "Monthly Time Series";
+                previous = "End of Last Month";
+                api = "TIME_SERIES_MONTHLY";
+                second_day = new Date(Date.now() - (86400000 * 30)); // TODO: Needs to be last day of previous month
 
-                    
-                } else {
-                    // TODO Better errors
-                    console.log(body);
-                    message.reply("There was an error fetching the ticker.");
-                }
-            });
+            default:
+                message.reply("Invalid period type. Please use 'm', 'd', or 'y'");
+                throw new Error();
         }
+
+        // Define time stamps we want to see from the time series data
+        let m = "";
+        m = first_day.getMonth()+1;
+        m = (m < 10 ? `0${m}` : m);
+        first_stamp  = `${first_day.getFullYear()}-${m}-${first_day.getDate()}`;
+        m = second_day.getMonth()+1;
+        m = (m < 10 ? `0${m}` : m);
+        second_stamp = `${second_day.getFullYear()}-${m}-${second_day.getDate()}`;
+
+        // Configure the request
+        var request = require('request');
+        var options = {
+            url: `${conf.apis.stocks.host}/query?function=${api}&symbol=${ticker_id}&apikey=${conf.apis.stocks.key}`,
+            method: 'GET',
+            headers: {},
+        }
+
+        // Start the request
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+                try {
+
+                    // Get data from response
+                    let obj    = JSON.parse(body);
+                    let meta   = obj['Meta Data'];
+                    let series = obj[chunk_index];
+
+                    let first_price  = parseFloat(series[first_stamp]["4. close"]);
+                    let second_price = parseFloat(series[second_stamp]["4. close"]);
+
+                    // Get percent change
+                    var change;
+                    var dir;
+                    var color;
+                    if(first_price > second_price) {
+                        dir = "+";
+                        color = 1222182; // Green
+                        change = (((first_price - second_price) / second_price) * 100).toFixed(2);
+                    } else {
+                        color = 13897487; // Red
+                        dir = "-";
+                        change = (((second_price - first_price) / second_price) * 100).toFixed(2);
+                    }
+                    
+                    // Format output
+                    let embed = new Discord.RichEmbed({
+                        "title": `💰 ${chunk} stock quote for ${ticker_id}`,
+                        "url": `https://www.google.com/search?q=stock%20quote%20${ticker_id}`,
+                        "color": color, 
+                        "footer": {"text": "Source: AlphaVantage.co API"},
+                        "fields": [
+                            {
+                                "name": "Latest Today",
+                                "value": `$${first_price} (${dir}${change}%)`,
+                                "inline": true
+                            },
+                            {
+                                "name": previous,
+                                "value": `$${second_price}`,
+                                "inline": true
+                            }
+                        ]
+                    }); 
+            
+                    // message.reply(`💰 **${ticker_id.toUpperCase()}**: \$${close_price}`); 
+                    message.channel.send("", embed);
+                }
+                catch(error) {
+                    console.log(error);
+                    message.reply("There was an error parsing the response.");  
+                }
+
+                
+            } else {
+                // TODO Better errors
+                console.log(body);
+                message.reply("There was an error fetching the ticker.");
+            }
+        });
+
 
     },
 };
